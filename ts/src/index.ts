fa4830cae528e13f48758f41c750243569ccdef5
adithsath <adithyanoffc@gmail.com>
2025-10-16 22:35:47 +0530

    Delete ts directory

--- ts/src/index.ts
+++
@@ -1,8 +0,0 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-if (require.main === module) {
-  console.log('Hello from ts folder');
-  console.log('3 + 4 =', add(3, 4));
-}
